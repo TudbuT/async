352ebb9727343f06ac1504e06a0c036389172f53
TudbuT <tudbut@tudbut.de>
2022-05-10 18:58:55 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-"use strict"; 
+"use strict";
+
+async function loop(array, call) {
+    async function step(n) {
+        if(n == array.length)
+            return;
+        await call(array[n]);
+        await new Promise((resolve) => setImmediate(async () => {await step(n+1); resolve()}));
+    }
+    await step(0);
+}
 
 async function stringify(object) {
     if(object === undefined)
@@ -12,44 +22,38 @@ async function stringify(object) {
                     return "[]";
                 }
                 let buf = "[";
-                for(let i = 0; i < object.length - 1; i++) {
+                await loop(object, async i => {
                     if(typeof object[i] === "function")
-                        continue;
+                        return;
                     buf += await stringify(object[i]);
                     buf += ",";
-                }
-                if(typeof object[object.length - 1] !== "function") {
-                    buf += await stringify(object[object.length - 1]);
-                }
+                });
+                buf = buf.substring(0, buf.length - 1);
                 buf += "]";
                 return buf;
             }
-            
+
             if(typeof object.toJSON === "function") {
                 return await stringify(object.toJSON());
             }
             if(object.toJSON !== undefined) {
                 return await stringify(object.toJSON);
             }
-            
+
             let keys = Object.keys(object);
             if(keys.length === 0) {
                 return "{}";
             }
             let buf = "{";
-            for(let i = 0; i < keys.length - 1; i++) {
-                if(typeof object[keys[i]] === "function") 
-                    continue;
+            await loop(keys, async i => {
+                if(typeof object[keys[i]] === "function")
+                    return;
                 buf += await stringify(keys[i]);
                 buf += ":";
                 buf += await stringify(object[keys[i]]);
                 buf += ",";
-            }
-            if(typeof object[keys[keys.length - 1]] !== "function") {
-                buf += await stringify(keys[keys.length - 1]);
-                buf += ":";
-                buf += await stringify(object[keys[keys.length - 1]]);
-            }
+            });
+            buf = buf.substring(0, buf.length - 1);
             buf += "}";
             return buf;
             break;
